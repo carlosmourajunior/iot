fe0dd2f4d55fb5434ef394a7c185cd1680cf0e30
linaro <carlos.moura@inatel.br>
2017-10-02 11:10:30 -0300

    Sensor de luminosidade Modulo 4

---
+++ sensor_luminosidade.py
@@ -0,0 +1,47 @@
+import spidev
+import time
+from libsoc import gpio
+
+from gpio_96boards import GPIO
+
+GPIO_CS = GPIO.gpio_id('GPIO_CS')
+LED = GPIO.gpio_id('GPIO_A')
+
+pins = ((GPIO_CS, 'out'), (LED, 'out'),)
+
+sensibilidade = 500
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 10000
+spi.mode = 0b00
+spi.bits_per_word = 8
+
+def readadc(gpio):
+
+	gpio.digital_write(GPIO_CS, GPIO.HIGH)
+	time.sleep(0.0002)
+	gpio.digital_write(GPIO_CS, GPIO.LOW)
+	r = spi.xfer2([0x01, 0xA0, 0x00])
+	gpio.digital_write(GPIO_CS, GPIO.HIGH)
+	adcout = (r[1] << 8) & 0b1100000000
+	adcout = adcout | (r[2] & 0xff)	
+
+	if adcout > sensibilidade:
+				
+		gpio.digital_write(LED, GPIO.HIGH)
+	else: 
+		gpio.digital_write(LED, GPIO.LOW)		
+	
+	print (adcout)
+	return r
+
+while True:
+	with GPIO(pins) as gpio:
+		value = readadc(gpio)
+		time.sleep(0.5)
+
+
+
+
+	
