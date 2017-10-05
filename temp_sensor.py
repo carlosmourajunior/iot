1ce00112f283da2d38016153d79ab715e2a7f087
linaro <carlos.moura@inatel.br>
2017-10-05 15:00:52 -0300

    Sensor de temperatura

--- temp_sensor.py
+++ temp_sensor.py
@@ -14,23 +14,26 @@ spi.max_speed_hz = 10000
 spi.mode = 0b00
 spi.bits_per_word = 8
 
-def readadc(gpio):
+def readtemp(gpio):
 
 	gpio.digital_write(GPIO_A, GPIO.HIGH)
 	time.sleep(0.0002)
 	gpio.digital_write(GPIO_A, GPIO.LOW)
 	r = spi.xfer2([0x01, 0xA0, 0x00])
 	gpio.digital_write(GPIO_A, GPIO.HIGH)
-	print (r)
 	adcout = (r[1] << 8) & 0b1100000000
 	adcout = adcout | (r[2] & 0xff)		
+
 	adc_temp = (adcout *5.0/1023-0.5)*100
-	print (adc_temp)
-	return r
+	
+	return adc_temp
+	
+
 
 while True:
 	with GPIO(pins) as gpio:
-		value = readadc(gpio)
+		value = readtemp(gpio)
+		print ("Temperatura: %2.1f" %value)
 		time.sleep(0.5)
 
 
