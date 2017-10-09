421121d4eb70a9bb24294a94d591676ad51212c1
Carlos Alberto P. Moura Jr <carlos.moura@inatel.br>
2017-10-09 09:19:01 -0300

    Tilt New

--- tilt.py
+++ tilt.py
@@ -11,8 +11,18 @@ pins = ((TILT, 'in'),)
 count = 0
 sleep_count = 0
 
+
+def detectTilt(gpio):
+	status = gpio.digital_read(TILT)
+	count = 0
+	tilt_detected = 0
+	while count < 1000:
+		if gpio.digital_read(TILT) != status:
+			tilt_detected += 1
+			print("Tilt Detected")
+			if tilt_detected > 5:
+				print("Problem Detected")
+
 with GPIO(pins) as gpio:
 	while True:
-
-		value = gpio.wait_for_interrupt(1)								
-		print("Contador:" , count)
+		detectTilt(gpio)
